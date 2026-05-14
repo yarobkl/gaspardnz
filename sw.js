61859babe3913d976e012537f5d4a9e8c16225b8
yarobkl <yarobkl@users.noreply.github.com>
2026-05-14 06:31:33 +0000

    deploy: b997b950823b87327c4c08fdaf6defe06d7b4820

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "gnz-v20260514-8835fcb";
+const VERSION = "gnz-v20260514-b997b95";
 
 self.addEventListener("install", () => self.skipWaiting());
 self.addEventListener("activate", e => e.waitUntil(
